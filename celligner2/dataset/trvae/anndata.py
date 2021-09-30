164d563f020b64dcbf0e99a28caff20d1a52e886
jkobject <jkobject@gmail.com>
2021-09-30 21:38:03 +0000

    finishing merge scArches

---
+++ celligner2/dataset/trvae/anndata.py
@@ -0,0 +1,121 @@
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from scipy import sparse
+
+from .data_handling import remove_sparsity
+from ._utils import label_encoder
+
+
+class AnnotatedDataset(Dataset):
+    """Dataset handler for TRVAE model and trainer.
+       Parameters
+       ----------
+       adata: : `~anndata.AnnData`
+            Annotated data matrix.
+       condition_key: String
+            column name of conditions in `adata.obs` data frame.
+       condition_encoder: Dict
+            dictionary of encoded conditions.
+       cell_type_keys: List
+            List of column names of different celltype hierarchies in `adata.obs` data frame.
+       cell_type_encoder: Dict
+            dictionary of encoded celltypes.
+    """
+    def __init__(self,
+                 adata,
+                 condition_key=None,
+                 condition_encoder=None,
+                 cell_type_keys=None,
+                 cell_type_encoder=None,
+                 ):
+
+        self.X_norm = None
+
+        self.condition_key = condition_key
+        self.condition_encoder = condition_encoder
+        self.cell_type_keys = cell_type_keys
+        self.cell_type_encoder = cell_type_encoder
+
+        if sparse.issparse(adata.X):
+            adata = remove_sparsity(adata)
+        self.data = torch.tensor(adata.X)
+
+        self.size_factors = torch.tensor(adata.obs['trvae_size_factors'])
+        self.labeled_vector = torch.tensor(adata.obs['trvae_labeled'])
+
+        # Encode condition strings to integer
+        if self.condition_key is not None:
+            self.conditions = label_encoder(
+                adata,
+                encoder=self.condition_encoder,
+                condition_key=condition_key,
+            )
+            self.conditions = torch.tensor(self.conditions, dtype=torch.long)
+
+        # Encode cell type strings to integer
+        if self.cell_type_keys is not None:
+            self.cell_types = list()
+            for cell_type_key in cell_type_keys:
+                level_cell_types = label_encoder(
+                    adata,
+                    encoder=self.cell_type_encoder,
+                    condition_key=cell_type_key,
+                )
+                self.cell_types.append(level_cell_types)
+
+            self.cell_types = np.stack(self.cell_types).T
+            self.cell_types = torch.tensor(self.cell_types, dtype=torch.long)
+
+    def __getitem__(self, index):
+        outputs = dict()
+
+        outputs["x"] = self.data[index, :]
+        outputs["labeled"] = self.labeled_vector[index]
+        outputs["sizefactor"] = self.size_factors[index]
+
+        if self.condition_key:
+            outputs["batch"] = self.conditions[index]
+
+        if self.cell_type_keys:
+            outputs["celltypes"] = self.cell_types[index, :]
+
+        return outputs
+
+    def __len__(self):
+        return self.data.size(0)
+
+    @property
+    def condition_label_encoder(self) -> dict:
+        return self.condition_encoder
+
+    @condition_label_encoder.setter
+    def condition_label_encoder(self, value: dict):
+        if value is not None:
+            self.condition_encoder = value
+
+    @property
+    def cell_type_label_encoder(self) -> dict:
+        return self.cell_type_encoder
+
+    @cell_type_label_encoder.setter
+    def cell_type_label_encoder(self, value: dict):
+        if value is not None:
+            self.cell_type_encoder = value
+
+    @property
+    def stratifier_weights(self):
+        conditions = self.conditions.detach().cpu().numpy()
+        condition_coeff = 1 / len(conditions)
+        weights_per_condition = list()
+        for i in range(len(self.conditions)):
+            samples_per_condition = np.count_nonzero(conditions == i)
+            if samples_per_condition == 0:
+                weights_per_condition.append(0)
+            else:
+                weights_per_condition.append((1 / samples_per_condition) * condition_coeff)
+        strat_weights = np.copy(conditions)
+        for i in range(len(conditions)):
+            strat_weights = np.where(strat_weights == i, weights_per_condition[i], strat_weights)
+
+        return strat_weights.astype(float)
---
+++ notes.md
@@ -0,0 +1,88 @@
+# Notes
+
+## Objectives
+
+### run scARCHES on CCLE + TCGA (use the batch annotation)
+
+### run scARCHES on CCLE + TCGA + CCLF + additional
+
+### semi supervision like mfMAP (need good annotations)
+
+- sample specific information (will need ashir's new annotations, also annotations for each dataset used)
+- label smoothing on semi-supervion (weak supervion)
+
+### do data augmentation thing: (see list in other document)
+
+- Use replicates when available.
+- Up to some random noise, based on how high is the expression (randomly add)
+- mix cell lines and normals to create fake impurity to correct
+  - do: ccle_line + impurity * gtex_tissue
+  - put semi supervision annotation as impurity: impurity, origin: gtex_tissue
+
+- add more datasets (list in asana)
+- add pseudo bulk from scRNAseq  (list in asana)
+
+- add mice data (once in graph)
+
+### add Ashir's annotation to the data and make it cross modal (add more QC)
+
+### make it graph like and deeper, and add edge prediction
+
+- use dgl's vgae
+- skip connections
+- add graph embedding learning and edge prediction
+  - drop links from model and train it to classify missing links (positive, negative)
+
+### what mfmap is not doing
+
+- no purity estimation?
+- not robust to outliers / not able to detect undif cluster
+- Maybe it should use gene network from cmap data.
+
+## what we would want to do
+
+min(A - (X_a*Y_a + I_a))
+min(B - (X_b*Y_b + I_b)) s.t. min(MIN(dist(X_a, X_b))) ; max(Y_a\*Y_b)
+
+## How to test
+
+- visually
+- confounding matrix (cell type distance (see allie's paper))
+- distance between known good similar lines.
+  - list of close matching lines using other paper's data and Allie's data (tumorComparer)
+  - fake tumor data (cell line + purity*normal)
+- ability to find out misslabeled lines:
+  - use list of putative mislabelled (outliers in the bioarxiv paper)
+  - create fake misslabeling
+- using HCMI's line
+- does known gene dependency of typical cancer lines match with clustering?
+- ask sanger for their RNAseq
+
+## ideas
+
+### random
+
+- celligner that uses gene loadings found by Josh's tool (Webster)
+- celligner is already working on subsets:
+  - set of genes are droped when cPCA, when mNN realignment
+
+### important
+
+- celligner that finds [a mapping / a cell line for a tumor] given a specific gene grouping (dependency/geneset..)
+- celligner that finds the best set of lines for all groupings for a cancer specific expression signature
+
+## paper architecture
+
+### introduction
+
+### the model
+
+### feature engineering
+
+#### results
+
+### comparison with Celligner v1, MFmap, other?
+
+### data
+
+## reproducibility (+badge)
